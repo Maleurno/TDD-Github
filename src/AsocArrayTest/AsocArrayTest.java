0066fcbe32fed20c08973f94db9668054d8e7bdc
maleurno <danielsivale@gmail.com>
2017-05-16 16:38:20 +0200

    Creo los paquetes AsocArray y AsocArrayTest y la clase JUnit

---
+++ src/AsocArrayTest/AsocArrayTest.java
@@ -0,0 +1,14 @@
+package AsocArrayTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AsocArrayTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
